3861b938d3aa7e07cd1c2c9bfe890440de08b7d8
elizeubarbosaabreu <elizeubcorreios@gmail.com>
2021-10-06 20:03:17 -0400

    Melhorias no Model do Projeto

--- agenda/settings.py
+++ agenda/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sxps0#7q6^qz6j2%rawdlfm3k-2hl0(ty=#t3_#6r6mf_eqx#@'
+SECRET_KEY = 'django-insecure-pcgs++$!hu4kkjjsp#e&)r5a7y@n56=)*u+78kmbi5+k_-uzg%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
+    'core',
 ]
 
 MIDDLEWARE = [
