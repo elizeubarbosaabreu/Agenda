797c163b28cfe789206d8651ca11d7e991d5d308
elizeubarbosaabreu <elizeubcorreios@gmail.com>
2021-10-13 21:37:18 -0400

    Configuração do timezone

--- core/views.py
+++ core/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect
 from core.models import Evento
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
-from django.contrib import messages 
+from django.contrib import messages
+from datetime import datetime, timedelta
 
 # Create your views here.
 
@@ -30,7 +31,9 @@ def submit_login(request):
 @login_required(login_url='/login/')
 def lista_eventos(request):
     usuario = request.user
-    evento = Evento.objects.filter(usuario=usuario)
+    data_atual = datetime.now() - timedelta(hours=1)
+    evento = Evento.objects.filter(usuario=usuario,
+                                   data_evento__gt=data_atual)
     dados = {'eventos': evento}
     return render(request, 'agenda.html', dados)
 
